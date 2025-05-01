02a7a35c175a76e4a69dfe80cd340de480d41059
Uri_CS <uriel2405correia@gmail.com>
2025-05-01 16:33:11 +0000

    Atualização final

--- Atividade_1/questao_1.c
+++ Atividade_1/questao_1.c
@@ -2,9 +2,30 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define TAM_STR 500
+#define TAM_COORD 300
+
+int procurando(char string[], int (*vec)[2]) {
+    int x = 0, y = 0;
+    int j = 0;
+
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (string[i] == '(') {
+            int offset;
+            if (sscanf(&string[i], "(%d,%d)%n", &x, &y, &offset) == 2) {
+                vec[j][0] = x;
+                vec[j][1] = y;
+                j++;
+                i += offset - 1; // evita reler o mesmo trecho
+            }
+        }
+    }
+    return j;
+}
+
 void ordenando(int (*vec)[2], int n){
 
-    int interm_1, interm_2;
+    int temp_x, temp_y;
 
     for(int i = 0; i < n; i++){
         for(int j = i + 1; j < n; j++){
@@ -12,14 +33,14 @@ void ordenando(int (*vec)[2], int n){
               (vec[i][0] == vec[j][0] && 
                vec[i][1] > vec[j][1])){
 
-                interm_1 = vec[i][0];
-                interm_2 = vec[i][1];
+                temp_x = vec[i][0];
+                temp_y = vec[i][1];
 
                 vec[i][0] = vec[j][0];
                 vec[i][1] = vec[j][1];
 
-                vec[j][0] = interm_1;
-                vec[j][1] = interm_2;
+                vec[j][0] = temp_x;
+                vec[j][1] = temp_y;
             }
         }
     }
@@ -35,23 +56,21 @@ void imprimindo(int (*vec)[2], int n){
 
 float distancia(int (*vec)[2], int n){
 
-    float soma_distance = 0.0;
+    float soma = 0.0;
 
     for(int i = 0; i < n - 1; i++){
 
         int dx = vec[i + 1][0] - vec[i][0]; 
         int dy = vec[i + 1][1] - vec[i][1];
         
-        soma_distance += sqrt(dx * dx + dy * dy);
+        soma += sqrt(dx * dx + dy * dy);
 
     }
 
-    return soma_distance;
+    return soma;
 }
 
 float distancia_extremos(int (*vec)[2], int n){
-
-    float shortcut = 0;
 
     int dx = vec[n - 1][0] - vec[0][0];
     int dy = vec[n - 1][1] - vec[0][1];
@@ -61,26 +80,19 @@ float distancia_extremos(int (*vec)[2], int n){
 
 int main(){
 
-    int n;
-    printf("Informe a quantidade coordenadas: ");
-    scanf("%d", &n);
+    char str[TAM_STR];
+    int vetor[TAM_COORD][2];
+
+    printf("Informe as coordenadas: ");
+    scanf(" %[^\n]", str);
     printf("\n");
 
-    int (*vetor)[2] = malloc (n * sizeof(int[2]));
+    int n = procurando(str, vetor);
 
-    if( vetor == NULL){
-        printf("Erro: Sem memória!\n");
+    if (n == 0) {
+        printf("Nenhuma coordenada válida encontrada.\n");
         return 1;
     }
-    
-    for(int i = 0; i < n; i++){
-        printf("Informe a coordenada %d de x: ", i + 1);
-        scanf("%d", &vetor[i][0]);
-        printf("Informe a coordenada %d de y: ", i + 1);
-        scanf("%d", &vetor[i][1]);
-    }
-
-    printf("\n");
 
     printf(">> Coordenadas na sequencia original: ");
     imprimindo(vetor, n);
@@ -95,8 +107,6 @@ int main(){
 
     float shortcut = distancia_extremos(vetor, n);
     printf(">> shortcut: %.2f\n", shortcut);
-
-    free(vetor);
 
     return 0;
 }
