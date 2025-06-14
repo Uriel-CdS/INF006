f547e8e3ecf406e82d13b8ee984d2bab56c2c337
Uri_CS <uriel2405correia@gmail.com>
2025-06-14 18:31:37 +0000

    Q1

--- Atividade_2/Q1.c
+++ Atividade_2/Q1.c
@@ -5,10 +5,19 @@
 
 #define MAX 100
 
-void ordering(int vector[], int qtd){
-    for(int i = 0; i < qtd - 1; i++){
-        for(int j = 0; j < qtd - i - 1; j++){
-            if(vector[j] > vector[j + 1]){
+typedef struct {
+    int numeros[MAX];
+    int qtd;
+    int soma;
+} Lista;
+
+Lista listas[MAX];
+int total_listas = 0;
+
+void ordenando(int vector[], int qtd) {
+    for (int i = 0; i < qtd - 1; i++) {
+        for (int j = 0; j < qtd - i - 1; j++) {
+            if (vector[j] > vector[j + 1]) {
                 int interm = vector[j + 1];
                 vector[j + 1] = vector[j];
                 vector[j] = interm;
@@ -17,56 +26,91 @@ void ordering(int vector[], int qtd){
     }
 }
 
-void capture(char entrance[]) {
-
-    int numbers[MAX];
-    int qtd = 0;
-
+int capturar_lista(char entrance[]) {
     char *ptr = entrance;
 
     while ((ptr = strstr(ptr, "start")) != NULL) {
-        ptr += 5; // Pula a palavra "start"
+        ptr += 5; // pula "start"
 
-        // Captura os números depois de "start" até achar outro "start" ou fim de linha
+        Lista nova;
+        nova.qtd = 0;
+
         while (*ptr) {
-            // Ignora espaços
             while (*ptr == ' ') ptr++;
 
-            // Se acharmos outra palavra "start", paramos esse bloco
             if (strncmp(ptr, "start", 5) == 0)
                 break;
 
             int num;
+            if (sscanf(ptr, "%d", &num) == 1) {
+                nova.numeros[nova.qtd++] = num;
+            }
 
-            sscanf(ptr, "%d", &num);
-
-            numbers[qtd++] = num;
-
-            // Avança o ponteiro até depois do número
             while (*ptr && !isspace(*ptr)) ptr++;
         }
 
-        // Ordena a lista atual
-        ordering(numbers, qtd);
+        ordenando(nova.numeros, nova.qtd);
 
-        printf("start");
-        for(int i = 0; i < qtd; i++){
-            printf(" %d", numbers[i]);
+        nova.soma = 0;
+        for (int i = 0; i < nova.qtd; i++)
+            nova.soma += nova.numeros[i];
+
+        listas[total_listas++] = nova;
+    }
+
+    return total_listas;
+}
+
+int remover_duplicadas(Lista listas[], int total) {
+    int usadas[2001] = {0};  // deslocamento de -1000 a +1000
+    Lista resultado[MAX];
+    int nova_qtd = 0;
+
+    for (int i = total - 1; i >= 0; i--) {
+        int soma = listas[i].soma;
+        if (!usadas[soma + 1000]) {
+            usadas[soma + 1000] = 1;
+            resultado[nova_qtd++] = listas[i];
         }
-        printf("\n");
+    }
 
-        qtd = 0; // Reseta o contador para a próxima lista
+    for (int i = 0; i < nova_qtd; i++) {
+        listas[i] = resultado[nova_qtd - 1 - i];
+    }
+
+    return nova_qtd;
+}
+
+void ordenar_lista_por_soma(Lista lista[], int total) {
+    for (int i = 0; i < total; i++) {
+        for (int j = 0; j < total - i - 1; j++) {
+            if (lista[j].soma < lista[j + 1].soma) {
+                Lista temp = lista[j];
+                lista[j] = lista[j + 1];
+                lista[j + 1] = temp;
+            }
+        }
     }
 }
 
-int main(){
-
+int main() {
     char str[1000];
 
     printf("Digite: ");
     scanf(" %[^\n]", str);
 
-    capture(str);
+    int total_listas = capturar_lista(str);
+
+    total_listas = remover_duplicadas(listas, total_listas);
+
+    ordenar_lista_por_soma(listas, total_listas);
+
+    for (int i = 0; i < total_listas; i++) {
+        printf("start");
+        for (int j = 0; j < listas[i].qtd; j++) {
+            printf(" %d", listas[i].numeros[j]);
+        }
+    }
 
     return 0;
 }
